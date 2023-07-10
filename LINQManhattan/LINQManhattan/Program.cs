865f9f2b4c973546ec9d180656b949210a389e05
Anas Al Shammah <anas.gharaibeh1@yahoo.com>
2023-07-10 20:03:04 +0300

    linq

---
+++ LINQManhattan/LINQManhattan/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+
+namespace LINQManhattan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var text = File.ReadAllText(@"./data.json");
+
+            Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(text);
+        
+            Feature[] features = rootObject.features;
+            var list =from feature in features
+                      select feature.properties.neighborhood;
+            var list2 = from n in list
+                        where n !=""
+                        select n;   
+            var list3 = list2.Distinct();
+            var list4 = (from feature in features
+                         where feature.properties.neighborhood != ""
+                         select feature.properties.neighborhood).Distinct();
+            var list5 = list.Where(e=>e!="").ToList();
+
+
+            foreach (var feature in list)
+            {
+                Console.WriteLine(feature);
+            }
+
+            foreach (var feature in list2)
+            {
+                Console.WriteLine(feature);
+            }
+
+            foreach (var feature in list3)
+            {
+                Console.WriteLine(feature);
+            }
+
+            foreach (var feature in list4)
+            {
+                Console.WriteLine(feature);
+            }
+
+            foreach (var feature in list5)
+            {
+                Console.WriteLine(feature);
+            }
+        }
+        public class Rootobject
+    {
+        public string type { get; set; }
+            public Feature[] features { get; set; }
+        }
+
+        public class Feature
+        {
+            public string type { get; set; }
+            public Geometry geometry { get; set; }
+            public Properties properties { get; set; }
+        }
+
+        public class Geometry
+        {
+            public string type { get; set; }
+            public float[] coordinates { get; set; }
+        }
+
+        public class Properties
+        {
+            public string zip { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string address { get; set; }
+            public string borough { get; set; }
+            public string neighborhood { get; set; }
+            public string county { get; set; }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # LINQ-in-Manhattan
+
+## Summary
+This program is designed to read data from an external JSON file and perform various filtering operations using LINQ queries. The JSON data contains location information for properties in Manhattan, and the program aims to answer specific questions based on the data.
+
+
+## Usage
+To use this program, follow these steps:
+
+- Clone or download the repository to your local machine.
+- Ensure you have the Newtonsoft.Json package installed in your C# project. If not, install it using NuGet.
+- Place the data.json file in the root folder of the project.
+- Build and run the program.
+
+The program will output the answers to the questions in the console.
+## Questions
+The program will answer the following questions:
+
+- Output all the neighborhoods in the data list.
+- Filter out neighborhoods without names.
+- Remove duplicate neighborhoods.
+- Rewrite the queries from the previous questions and consolidate them into a single query.
+- Rewrite at least one question using the opposing LINQ method.
